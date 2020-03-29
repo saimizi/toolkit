f77114a27486b211a8e4c9de4c708e79edfdbcaf
Seimizu Joukan <saimizi@gmail.com>
2020-03-29 18:15:19 +0900

    Update gu.go

--- gu.go
+++ gu.go
@@ -11,6 +11,8 @@ import (
 	"strings"
 )
 
+var formatPretty bool = false
+
 func code(depthList ...int) string {
 	var depth int
 
@@ -29,12 +31,15 @@ func main() {
 
 	boolptrShowHide := flag.Bool("showHide", false, "Show hidden files.")
 	boolptrShowPath := flag.Bool("showPath", false, "Show hidden files.")
+	boolptrPretty := flag.Bool("pretty", false, "Show hidden files.")
 
 	flag.Parse()
 
 	if *boolptrShowHide {
 		ShowFlag = SHOWFLAG_ALL
 	}
+
+	formatPretty = *boolptrPretty
 
 	topath := flag.Args()
 
@@ -50,39 +55,67 @@ func main() {
 
 func printFiles(msg string, fs []string) {
 	const (
-		maxfiles int = 100
+		maxfiles    int = 100
+		filePerLine int = 5
+		lenPerLine  int = 75
 	)
+
+	c := color.New(color.FgWhite)
+	switch msg {
+	case "D":
+		c = color.New(color.FgBlue)
+	case "H":
+		c = color.New(color.FgCyan)
+	case "Z":
+		c = color.New(color.FgRed)
+	case "P":
+		c = color.New(color.FgMagenta)
+	case "E":
+		c = color.New(color.FgGreen)
+	default:
+	}
 
 	if len(fs) == 0 {
 		return
 	}
 
-	fmt.Printf("%s (%d):", msg, len(fs))
-	if len(fs) > 1000 {
-		fmt.Printf("\n  ")
+	if formatPretty {
+		fmt.Printf("%s (%d):\n", msg, len(fs))
+		i := 0
+		lenline := 0
+
+		for _, f := range fs {
+			i++
+			if i > maxfiles {
+				break
+			}
+
+			c.Printf("%-16s\t", f)
+			lenline += len(f)
+
+			if lenline > lenPerLine || (i%filePerLine) == 0 {
+				if i < len(fs) {
+					c.Println()
+				}
+				lenline = 0
+			}
+		}
 	} else {
-		fmt.Printf(" ")
-	}
-	for i, f := range fs {
-		c := color.New(color.FgWhite)
-
-		if msg == "D" {
-			c = color.New(color.FgBlue)
+		fmt.Printf("%s (%d):", msg, len(fs))
+		if len(fs) > 1000 {
+			fmt.Printf("\n  ")
+		} else {
+			fmt.Printf(" ")
 		}
 
-		if msg == "H" {
-			c = color.New(color.FgCyan)
-		}
-
-		if msg == "E" {
-			c = color.New(color.FgGreen)
-		}
-		c.Printf("%s ", f)
-
-		if i > maxfiles {
-			break
+		for i, f := range fs {
+			c.Printf("%s ", f)
+			if i > maxfiles {
+				break
+			}
 		}
 	}
+
 	fmt.Println()
 }
 
@@ -91,12 +124,14 @@ const (
 	SHOWFLAG_D   = 0x1 << 2
 	SHOWFLAG_E   = 0x1 << 3
 	SHOWFLAG_N   = 0x1 << 4
-	SHOWFLAG_ALL = SHOWFLAG_H | SHOWFLAG_D | SHOWFLAG_E | SHOWFLAG_N
+	SHOWFLAG_Z   = 0x1 << 5
+	SHOWFLAG_P   = 0x1 << 6
+	SHOWFLAG_ALL = SHOWFLAG_H | SHOWFLAG_D | SHOWFLAG_Z | SHOWFLAG_P | SHOWFLAG_E | SHOWFLAG_N
 )
 
 func showCurrentDir(bitflag int) {
 	var err string
-	var hide, dir, execfile, file []string
+	var hide, dir, zfile, pfile, execfile, file []string
 
 	for {
 		curr, err := os.Getwd()
@@ -118,6 +153,19 @@ func showCurrentDir(bitflag int) {
 
 			if f.IsDir() {
 				dir = append(dir, f.Name()+"/")
+				continue
+			}
+
+			if strings.HasSuffix(f.Name(), ".tar.gz") ||
+				strings.HasSuffix(f.Name(), ".tar.bz2") ||
+				strings.HasSuffix(f.Name(), ".zip") {
+				zfile = append(zfile, f.Name())
+				continue
+			}
+
+			if strings.HasSuffix(f.Name(), ".png") ||
+				strings.HasSuffix(f.Name(), ".jpg") {
+				pfile = append(pfile, f.Name())
 				continue
 			}
 
@@ -145,6 +193,25 @@ func showCurrentDir(bitflag int) {
 			sort.Slice(dir, fn)
 			printFiles("D", dir)
 		}
+
+		if (bitflag & SHOWFLAG_Z) != 0 {
+			fn := func(i, j int) bool {
+				return zfile[i] < zfile[j]
+			}
+
+			sort.Slice(zfile, fn)
+			printFiles("Z", zfile)
+		}
+
+		if (bitflag & SHOWFLAG_P) != 0 {
+			fn := func(i, j int) bool {
+				return pfile[i] < pfile[j]
+			}
+
+			sort.Slice(pfile, fn)
+			printFiles("P", pfile)
+		}
+
 		if (bitflag & SHOWFLAG_E) != 0 {
 			fn := func(i, j int) bool {
 				return execfile[i] < execfile[j]
@@ -153,6 +220,7 @@ func showCurrentDir(bitflag int) {
 			sort.Slice(execfile, fn)
 			printFiles("E", execfile)
 		}
+
 		if (bitflag & SHOWFLAG_N) != 0 {
 			fn := func(i, j int) bool {
 				return file[i] < file[j]
